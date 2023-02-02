1649ddf3b87b7cb580d4771241ccfc42c1c41f5c
Alireza <alireza.sheikh76@gmail.com>
2023-02-02 20:52:53 +0330

    Delete unnecessary prints

--- Model.py
+++ Model.py
@@ -1,8 +1,6 @@
 from tensorflow.keras import layers
 import tensorflow_addons as tfa
-
-
-
+from tensorflow import keras
 
 class Conv_block(layers.Layer):
     def __init__(self, out_channels, kernel_size=(3, 3), dilation_rate=1):
@@ -23,13 +21,11 @@ class Conv_block(layers.Layer):
         x = self.LeakyRelu(x)
         return x
 
-
 class SA_Block(layers.Layer):
     def __init__(self, out_channels):
         super(SA_Block, self).__init__()  # SA_Block or Conv_block
         self.conv1 = Conv_block(out_channels=out_channels, kernel_size=3)
         self.conv2 = Conv_block(out_channels=out_channels, kernel_size=3)
-
         self.out_channels = out_channels
         self.AVGpool = layers.AveragePooling2D(pool_size=(2, 2), strides=2)
         self.Upsample = layers.UpSampling2D(size=(2, 2))
@@ -39,14 +35,12 @@ class SA_Block(layers.Layer):
     def call(self, input):
         x_res = self.conv1(input)
         y_res = self.conv2(x_res)
-
         x_atten = self.AVGpool(input)
         x_atten = self.conv1(x_atten)
-        x_attne = self.conv2(x_atten)
+        x_atten = self.conv2(x_atten)
         y_attention = self.Upsample(x_atten)
         out_mul = self.multiply([y_attention, y_res])
         out = self.add([out_mul, y_attention])
-
         return out
 
 
@@ -71,11 +65,11 @@ class Upsample(layers.Layer):
         super(Upsample, self).__init__()
         self.upsampling = layers.UpSampling2D(size=(2, 2), interpolation='bilinear')
         self.conv = Conv_block(out_channels, kernel_size=(1, 1))
+
     def call(self, input):
         x = self.upsampling(input)
         x = self.conv(x)
         return x
-
 
 class DASPP(layers.Layer):
     def __init__(self, out_channels):
@@ -89,6 +83,7 @@ class DASPP(layers.Layer):
         self.upsampling = layers.UpSampling2D
         self.concat = layers.Concatenate(axis=-1)
         self.conv_block_out = Conv_block(out_channels, kernel_size=(1, 1), dilation_rate=1)
+
     def call(self, input):
         dims = input.shape
         x_pool = self.avgpool(pool_size=(dims[-3], dims[-2]))(input)
@@ -105,75 +100,57 @@ class DASPP(layers.Layer):
         return output
 
 class MyModel(keras.Model):
-  def __init__(self, num_classes):
-    super(MyModel, self).__init__()
-    self.concat = layers.Concatenate(axis=-1)
-    self.Up = Upsample   
-    self.Down = Downsample
-    self.DASPP = DASPP
-    self.SA = SA_Block
-    self.conv = layers.Conv2D
-    self.num_classes = num_classes
+    def __init__(self, num_classes):
+        super(MyModel, self).__init__()
+        self.concat = layers.Concatenate(axis=-1)
+        self.Up = Upsample   
+        self.Down = Downsample
+        self.DASPP = DASPP
+        self.SA = SA_Block
+        self.conv = layers.Conv2D
+        self.num_classes = num_classes
 
-  def call(self, input):
-    x1 = SA_Block(32)(input)
-    x2 = self.Down(32)(x1)
-    print("Stage1", x2.shape)
+    def call(self, input):
+        x1 = SA_Block(32)(input)
+        x2 = self.Down(32)(x1)
 
-    x3 = self.SA(64)(x2)
-    x4 = self.Down(64)(x3)
-    print("Stage2", x4.shape)
+        x3 = self.SA(64)(x2)
+        x4 = self.Down(64)(x3)
 
-    x5 = self.SA(128)(x4)
-    x6 = self.Down(128)(x5)
-    print("Stage3", x6.shape)
+        x5 = self.SA(128)(x4)
+        x6 = self.Down(128)(x5)
 
-    x7 = self.SA(256)(x6)
-    x8 = self.Down(256)(x7)
-    print("Stage4", x8.shape)
+        x7 = self.SA(256)(x6)
+        x8 = self.Down(256)(x7)
 
-    x9 = self.SA(512)(x8)
-    print("Stage5", x9.shape)
-    
-    x10 = self.DASPP(256)(x9)
-    print("Stage6", x10.shape)
+        x9 = self.SA(512)(x8)
+        
+        x10 = self.DASPP(256)(x9)
 
-    x11 = self.Up(256)(x10)
-    print("Stage7", x11.shape)
-    x12 = self.concat([x11, x7])
-    print("Stage7_1", x12.shape)
-    x13 = self.SA(256)(x12)
-    print("Stage8", x13.shape)
+        x11 = self.Up(256)(x10)
+        x12 = self.concat([x11, x7])
+        x13 = self.SA(256)(x12)
 
-    x14 = self.Up(128)(x13)
-    print("Stage9", x14.shape)
-    x15 = self.concat([x14, x5])
-    print("Stage9_1", x15.shape)
-    x16 = self.SA(128)(x15)
-    print("Stage10", x16.shape)
+        x14 = self.Up(128)(x13)
+        x15 = self.concat([x14, x5])
+        x16 = self.SA(128)(x15)
 
-    x17 = self.Up(64)(x16)
-    print("Stage11", x17.shape)
-    x18 = self.concat([x17, x3])
-    print("Stage11_1", x18.shape)
-    x19 = self.SA(64)(x18)
-    print("Stage12", x19.shape)
+        x17 = self.Up(64)(x16)
+        x18 = self.concat([x17, x3])
+        x19 = self.SA(64)(x18)
 
-    x20 = self.Up(32)(x19)
-    print("Stage13", x20.shape)
-    x21 = self.concat([x20, x1])
-    print("Stage13_1", x21.shape)
-    x22 = self.SA(32)(x21)
-    print("Stage14", x22.shape)
+        x20 = self.Up(32)(x19)
+        x21 = self.concat([x20, x1])
+        x22 = self.SA(32)(x21)
 
-    if self.num_classes == 2:
-      activation = 'sigmoid'
+        if self.num_classes == 2:
+            activation = 'sigmoid'
 
-    else:
-      activation = 'softmax'
-    
-    out = self.conv(filters=self.num_classes, kernel_size=3, 
-                                padding="same", use_bias=True, 
-                                activation=activation)(x22)
-                                
-    return out
+        else:
+            activation = 'softmax'
+        
+        out = self.conv(filters=self.num_classes, kernel_size=3, 
+                                    padding="same", use_bias=True, 
+                                    activation=activation)(x22)
+
+        return out
